97e75e137cec24faf2d7f166c919e421e6cc8907
Jed Brown <jed@59A2.org>
2008-07-22 00:28:26 +0000

    Creating meshes of regular hexes and explicitly orienting faces and edges.

---
+++ sandbox/itaps/dohpblock.c
@@ -0,0 +1,358 @@
+static const char help[] = "Enhance a mesh with hex element and vertices to include explicit reference to facets.\n";
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "iMesh.h"
+#include "petsc.h"
+#include "dohpmesh.h"
+
+#define CHK(err) if (err) { printf("iMesh error at %s:%d\n", __FILE__, __LINE__); exit(1); }
+#define ERR(str) { printf("error at %s:%d ",__FILE__,__LINE__); exit(2); }
+#define NAME_LEN 256
+#define DATA_LEN 256
+
+/* Assign a canonical orientation of the faces on a hex.  The ordering of faces
+* and vertices of the hex is defined by iMesh.  The ordering of edges and
+* vertices of the faces are also defined.  We need to find out which rotation of
+* the face (in 3D) is required so that the face is in standard orientation.
+* Equivalently, we need to know how to compute loop bounds on the face dofs so
+* that they are traversed in the forward order when the hex face dofs are
+* traversed in forward order.  We will need the permutation P such that for face 'i'
+*   region_vertex[hexquad[i][j]] = face_vertex[P[j]]
+* */
+const int hexquad[6][4] = {{0,1,5,4}, {1,2,6,5}, {2,3,7,6}, {3,0,4,7}, {0,3,2,1}, {4,5,6,7}};
+
+/* Assign a canonical orientation of the edges of a quad.  This is a much simpler analogue of hexquad.
+* We will need the permutation P such that for edge 'i'
+*   face_vertex[quadline[i][j]] = edge_vertex[P[j]]
+* */
+const int quadline[4][2] = {{0,1},{1,2},{2,3},{3,4}};
+
+#if (USE_ORIENT_ENUM)
+#undef __FUNCT__
+#define __FUNCT__ "DohpLoopBounds_Face"
+/* Get the loop bounds for a face of order 'ford' to traverse the edge number
+* 'eind' of order 'eord' in the canonical direction.
+*
+*   D -- 2 -- C
+*   |         |
+*   3         1
+*   |         |
+*   A -- 0 -- B
+*
+* degrees of freedom are numbered:
+* 0                   1  ... ford[1]-1
+* ford[1] ...
+* (ford[0]-1)*ford[1] .  ... ford[0]*ford[1]-1
+*/
+PetscErrorCode DohpLoopBounds_Quad(PetscInt dof, PetscInt foff, const PetscInt *ford,
+                                   PetscInt eind, DohpOrient eorient, const PetscInt *eord,
+                                   PetscInt *start, PetscInt *stride, PetscInt *end)
+{
+  const PetscInt x_rev = eorient & ORIENT_X_REV, x_low = eorient & ORIENT_X_LOW, x_high = eorient & ORIENT_X_HIGH;
+  const PetscInt a = foff + (ford[0]-1)*ford[1]*dof, b = foff + (ford[0]*ford[1]-1)*dof;
+  const PetscInt c = foff + (ford[1]-1)*dof, d = 0;
+#define SET_BOUNDS(alt, dof, start0, stride0, end0, start1, stride1, end1) \
+  if (alt) { *start = (start0); *stride = (dof)*(stride0); *end = (end0) + (dof)*(stride0); } \
+  else { *start = (start1); *stride = (dof)*(stride1); *end = (end1) + (dof)*(stride1); }
+
+  PetscFunctionBegin;
+  if (x_low && x_high) SETERRQ(1, "Invalid DohpOrient: ORIENT_X_LOW and ORIENT_X_HIGH are both set.");
+  if (x_low || x_high) SETERRQ(1, "Loop bounds for nonconforming Quad not implemented.");
+  switch (eind) {
+    case 0: SET_BOUNDS(!x_rev, dof, a,        1, b, b,       -1, a); break;
+    case 1: SET_BOUNDS(!x_rev, dof, b, -ford[1], c, c,  ford[1], b); break;
+    case 2: SET_BOUNDS(!x_rev, dof, c,       -1, d, d,        1, c); break;
+    case 3: SET_BOUNDS(!x_rev, dof, d,  ford[1], a, a, -ford[1], d); break;
+    default: SETERRQ1(1, "Invalid edge index %d", eind);
+  }
+  PetscFunctionReturn(0);
+}
+#endif
+
+#undef __FUNCT__
+#define __FUNCT__ "DohpOrientFindPerm_HexQuad"
+PetscErrorCode DohpOrientFindPerm_HexQuad(const iBase_EntityHandle *rv, const iBase_EntityHandle *fv,
+                                          int fnum, DohpOrient *orient)
+{
+  PetscInt i;
+  const PetscInt perm[8][4] = {{0,1,2,3},{1,2,3,0},{2,3,0,1},{3,0,1,2},
+                               {0,3,2,1},{3,2,1,0},{2,1,0,3},{1,0,3,2}};
+  const DohpOrient permorient[8] = {0,1,2,3,4,5,6,7};
+
+  PetscFunctionBegin;
+#if 0
+  printf("# rv:");              /* The vertices of face fnum on this region in canonical order. */
+  for (i=0; i<4; i++) { printf(" %ld",(long)rv[hexquad[fnum][i]]); }
+  printf("\n# fv:");
+  /* The order of vertices on the face.  We want to find a permutation of these
+  vertices to matches the canonical order. */
+  for (i=0; i<4; i++) { printf(" %ld",(long)fv[i]); }
+  printf("\n");
+#endif
+  for (i=0; i<8; i++) { /* loop over permutations */
+    if (fv[perm[i][0]] == rv[hexquad[fnum][0]] && fv[perm[i][1]] == rv[hexquad[fnum][1]]) {
+      /* we have found a match, as an extra check we can check that the other vertices match */
+      if (fv[perm[i][2]] != rv[hexquad[fnum][2]] || fv[perm[i][3]] != rv[hexquad[fnum][3]]) {
+        SETERRQ(1,"Faces cannot be matched.");
+      }
+      *orient = permorient[i];
+      break;
+    }
+  }
+  if (i==8) SETERRQ(1,"Faces cannot be matched.");
+  PetscFunctionReturn(0);
+}
+
+#undef __FUNCT__
+#define __FUNCT__ "DohpOrientFindPerm_QuadLine"
+PetscErrorCode DohpOrientFindPerm_QuadLine(const iBase_EntityHandle *fv, const iBase_EntityHandle *ev,
+                                          int en, DohpOrient *orient)
+{
+  const PetscInt perm[2][2] = {{0,1},{1,0}};
+  const DohpOrient permorient[4] = {0,1,2,3};
+  PetscInt i;
+
+  PetscFunctionBegin;
+#if 0
+  printf("# fv:");              /* The vertices of edge en on this face in canonical order. */
+  for (i=0; i<2; i++) { printf(" %ld",(long)fv[quadline[en][i]]); }
+  printf("\n# ev:");
+  /* The order of vertices on the edge.  We want to find a permutation of these
+  vertices to matches the canonical order. */
+  for (i=0; i<2; i++) { printf(" %ld",(long)ev[i]); }
+  printf("\n");
+#endif
+  for (i=0; i<2; i++) { /* loop over permutations */
+    if (ev[perm[i][0]] == fv[quadline[en][0]] && ev[perm[i][1]] == fv[quadline[en][1]]) {
+      *orient = permorient[i];
+      break;
+    }
+  }
+  if (i==4) SETERRQ(1,"Edges cannot be matched.");
+  PetscFunctionReturn(0);
+}
+
+
+#undef __FUNCT__
+#define __FUNCT__ "main"
+int main(int argc, char *argv[])
+{
+  const char *dflt_outfile="dblock.h5m", *outopts="", *pTagName="MATERIAL_SET";
+  iMesh_Instance mesh;
+  iBase_EntitySetHandle root;
+  iBase_TagHandle pTag,feOrientTag,feAdjTag,rfOrientTag,rfAdjTag;
+  MeshListEH v={0},e={0},f={0},r={0},c={0},ev={0},fv={0},rv={0};
+  MeshListDouble x={0};
+  MeshListInt s={0},part={0},feo={0},in={0},fvo={0},evo={0},rfo={0},rvo={0};
+  DohpOrient *feOrient,*rfOrient;
+  PetscInt feOrientSize,rfOrientSize;
+  const char *outfile;
+  int err,ierr,i,j,k,m,n,p,M,N,P,I,J,K,order=iBase_INTERLEAVED;
+  double x0,x1,y0,y1,z0,z1;
+
+  PetscFunctionBegin;
+  ierr = PetscInitialize(&argc,&argv,(char *)0,help);CHKERRQ(ierr);
+  iMesh_newMesh("", &mesh, &err, 0);CHK(err);
+  iMesh_getRootSet(mesh, &root, &err);CHK(err);
+  if (argc < 4 || argc > 5) { printf("usage: %s x0:x1,y0:y1,z0:z1 m,n,p M,N,P [outfile]\n",argv[0]); exit(1); }
+  i = sscanf(argv[1],"%lf:%lf,%lf:%lf,%lf:%lf",&x0,&x1,&y0,&y1,&z0,&z1);
+  if (i != 6) SETERRQ(1,"Failed to parse bounding box.");
+  i = sscanf(argv[2],"%d,%d,%d",&m,&n,&p);
+  if (i != 3) SETERRQ(1,"Failed to parse size.");
+  i = sscanf(argv[3],"%d,%d,%d",&M,&N,&P);
+  if (i != 3) SETERRQ(1,"Failed to parse partition size.");
+  outfile = (argc == 5) ? argv[4] : dflt_outfile;
+
+  /* Create vertices */
+  x.a = x.s = m*n*p*3; x.v = malloc(x.a*sizeof(double));
+  for (i=0; i<m; i++) {
+    for (j=0; j<n; j++) {
+      for (k=0; k<p; k++) {
+        I = (i*n+j)*p+k;
+        x.v[3*I+0] = x0 + (x1-x0)*(1.*i/(m-1));
+        x.v[3*I+1] = y0 + (y1-y0)*(1.*j/(n-1));
+        x.v[3*I+2] = z0 + (z1-z0)*(1.*k/(p-1));
+      }
+    }
+  }
+  iMesh_createVtxArr(mesh,m*n*p,order,x.v,x.s,&v.v,&v.a,&v.s,&err);CHK(err);
+  MeshListFree(x);
+
+  /* Create regions */
+  c.a = c.s = (m-1)*(n-1)*(p-1)*8; c.v = malloc(c.a*sizeof(iBase_EntityHandle)); /* connectivity */
+  I=0;
+  for (i=0; i<m-1; i++) {
+    for (j=0; j<n-1; j++) {
+      for (k=0; k<p-1; k++) {
+        c.v[I++] = v.v[((i+0)*n+(j+0))*p+(k+0)];
+        c.v[I++] = v.v[((i+1)*n+(j+0))*p+(k+0)];
+        c.v[I++] = v.v[((i+1)*n+(j+1))*p+(k+0)];
+        c.v[I++] = v.v[((i+0)*n+(j+1))*p+(k+0)];
+        c.v[I++] = v.v[((i+0)*n+(j+0))*p+(k+1)];
+        c.v[I++] = v.v[((i+1)*n+(j+0))*p+(k+1)];
+        c.v[I++] = v.v[((i+1)*n+(j+1))*p+(k+1)];
+        c.v[I++] = v.v[((i+0)*n+(j+1))*p+(k+1)];
+      }
+    }
+  }
+  if (I != c.s) SETERRQ(1,"Wrong number of regions.");
+  iMesh_createEntArr(mesh,iMesh_HEXAHEDRON,c.v,c.s,&r.v,&r.a,&r.s,&s.v,&s.a,&s.s,&err);CHK(err);
+  if (r.s != (m-1)*(n-1)*(p-1)) SETERRQ(1,"Wrong number of regions created.");
+  printf("region size %d, status size %d\n",r.s,s.s);
+  /* Create partition. */
+  part.a = part.s = r.s; part.v = malloc(part.a*sizeof(int));
+  for (i=0; i<m-1; i++) {
+    for (j=0; j<n-1; j++) {
+      for (k=0; k<p-1; k++) {
+        I = i*M/(m-1); J = j*N/(n-1); K = k*P/(p-1);
+        part.v[(i*(n-1)+j)*(p-1)+k] = (I*N+J)*P+K + 1; /* Add 1 because MATERIAL_SET counts from 1 */
+      }
+    }
+  }
+  /* MATERIAL_SET is a special name associated with all iMesh instances
+  * If we are using a different name, we can assume it is not special. */
+  if (strcmp(pTagName,"MATERIAL_SET")) {
+    iMesh_createTag(mesh,pTagName,1,iBase_INTEGER,&pTag,&err,sizeof(pTagName));CHK(err);
+  } else {
+    iMesh_getTagHandle(mesh,"MATERIAL_SET",&pTag,&err,strlen("MATERIAL_SET"));CHK(err);
+  }
+  iMesh_setIntArrData(mesh,r.v,r.s,pTag,part.v,part.s,&err);CHK(err);
+  MeshListFree(r); MeshListFree(s); MeshListFree(c); MeshListFree(part);
+
+  /* Create faces */
+  c.a = c.s = 4*((m-1)*(n-1)*p + (m-1)*n*(p-1) + m*(n-1)*(p-1)); c.v = malloc(c.a*sizeof(iBase_EntityHandle));
+  I = 0;
+  for (i=0; i<m-1; i++) {
+    for (j=0; j<n-1; j++) {
+      for (k=0; k<p; k++) {
+        c.v[I++] = v.v[((i+0)*n+(j+0))*p+k];
+        c.v[I++] = v.v[((i+1)*n+(j+0))*p+k];
+        c.v[I++] = v.v[((i+1)*n+(j+1))*p+k];
+        c.v[I++] = v.v[((i+0)*n+(j+1))*p+k];
+      }
+    }
+  }
+  for (i=0; i<m-1; i++) {
+    for (j=0; j<n; j++) {
+      for (k=0; k<p-1; k++) {
+        c.v[I++] = v.v[((i+0)*n+j)*p+(k+0)];
+        c.v[I++] = v.v[((i+1)*n+j)*p+(k+0)];
+        c.v[I++] = v.v[((i+1)*n+j)*p+(k+1)];
+        c.v[I++] = v.v[((i+0)*n+j)*p+(k+1)];
+      }
+    }
+  }
+  for (i=0; i<m; i++) {
+    for (j=0; j<n-1; j++) {
+      for (k=0; k<p-1; k++) {
+        c.v[I++] = v.v[(i*n+(j+0))*p+(k+0)];
+        c.v[I++] = v.v[(i*n+(j+1))*p+(k+0)];
+        c.v[I++] = v.v[(i*n+(j+1))*p+(k+1)];
+        c.v[I++] = v.v[(i*n+(j+0))*p+(k+1)];
+      }
+    }
+  }
+  if (I != c.s) SETERRQ(1, "Wrong number of faces.");
+  iMesh_createEntArr(mesh,iMesh_QUADRILATERAL,c.v,c.s,&f.v,&f.a,&f.s,&s.v,&s.a,&s.s,&err);CHK(err);
+  printf("face size %d, status size %d\n",f.s,s.s);
+  MeshListFree(f); MeshListFree(s); MeshListFree(c);
+
+  /* Create edges */
+  c.a = c.s = 2*(m*n*(p-1) + m*(n-1)*p + (m-1)*n*p); c.v = malloc(c.a*sizeof(iBase_EntityHandle));
+  I = 0;
+  for (i=0; i<m; i++) {
+    for (j=0; j<n; j++) {
+      for (k=0; k<p-1; k++) {
+        c.v[I++] = v.v[(i*n+j)*p+(k+0)];
+        c.v[I++] = v.v[(i*n+j)*p+(k+1)];
+      }
+    }
+  }
+  for (i=0; i<m; i++) {
+    for (j=0; j<n-1; j++) {
+      for (k=0; k<p; k++) {
+        c.v[I++] = v.v[(i*n+(j+0))*p+k];
+        c.v[I++] = v.v[(i*n+(j+1))*p+k];
+      }
+    }
+  }
+  for (i=0; i<m-1; i++) {
+    for (j=0; j<n; j++) {
+      for (k=0; k<p; k++) {
+        c.v[I++] = v.v[((i+0)*n+j)*p+k];
+        c.v[I++] = v.v[((i+1)*n+j)*p+k];
+      }
+    }
+  }
+  if (I != c.s) SETERRQ(1, "Wrong number of edges.");
+  iMesh_createEntArr(mesh,iMesh_LINE_SEGMENT,c.v,c.s, &e.v,&e.a,&e.s, &s.v,&s.a,&s.s,&err);CHK(err);
+  printf("edge size %d, status size %d\n",e.s,s.s);
+  MeshListFree(e); MeshListFree(s); MeshListFree(c);
+
+  /* We are done with the master vertex record and ready to set up orientations. */
+  MeshListFree(v);
+
+  /* Orient Edges with respect to Faces */
+  iMesh_getEntities(mesh,0,iBase_FACE,iMesh_QUADRILATERAL,&f.v,&f.a,&f.s,&err);CHK(err);
+  iMesh_getAdjEntities(mesh,0,iBase_FACE,iMesh_QUADRILATERAL,iBase_EDGE,
+                       &e.v,&e.a,&e.s, &feo.v,&feo.a,&feo.s, &in.v,&in.a,&in.s, &err);CHK(err);
+  MeshListFree(in);
+  iMesh_getEntArrAdj(mesh,f.v,f.s,iBase_VERTEX,&fv.v,&fv.a,&fv.s,&fvo.v,&fvo.a,&fvo.s,&err);CHK(err);
+  iMesh_getEntArrAdj(mesh,e.v,e.s,iBase_VERTEX,&ev.v,&ev.a,&ev.s,&evo.v,&evo.a,&evo.s,&err);CHK(err);
+  printf("f %d, e %d, fv %d, feo %d, fvo %d, ev %d, evo %d\n", f.s,e.s,fv.s,feo.s,fvo.s,ev.s,evo.s);
+  feOrientSize = e.s;
+  ierr = PetscMalloc(feOrientSize*sizeof(DohpOrient),&feOrient);CHKERRQ(ierr);
+  for (i=0; i<f.s; i++) {      /* Loop over faces */
+    printf("face[%d] vertex: %ld %ld %ld %ld\n",i,
+           (long)fv.v[fvo.v[i]],  (long)fv.v[fvo.v[i]+1],
+           (long)fv.v[fvo.v[i]+2],(long)fv.v[fvo.v[i]+3]);
+    for (j=0; j<4; j++) {       /* Loop over edges adjacent to this face */
+      printf("edge[%d][%d] %ld %ld\n",i,j,(long)ev.v[evo.v[feo.v[i]+j]],(long)ev.v[evo.v[feo.v[i]+j]+1]);
+      ierr = DohpOrientFindPerm_QuadLine(&fv.v[fvo.v[i]],&ev.v[evo.v[feo.v[i]+j]],j,&feOrient[feo.v[i]+j]);CHKERRQ(ierr);
+    }
+  }
+  iMesh_createTag(mesh,DOHP_ORIENT_FACE_EDGE,4*sizeof(DohpOrient),iBase_BYTES,
+                  &feOrientTag,&err,strlen(DOHP_ORIENT_FACE_EDGE));CHK(err);
+  iMesh_setArrData(mesh,f.v,f.s,feOrientTag,(const char*)feOrient,feOrientSize*sizeof(DohpOrient),&err);CHK(err);
+  iMesh_createTag(mesh,DOHP_ADJ_FACE_EDGE,4,iBase_ENTITY_HANDLE,&feAdjTag,&err,strlen(DOHP_ADJ_FACE_EDGE));CHK(err);
+  iMesh_setEHArrData(mesh,f.v,f.s,feAdjTag,e.v,e.s,&err);CHK(err);
+
+  MeshListFree(f); MeshListFree(e); MeshListFree(feo); MeshListFree(fv); MeshListFree(fvo);
+  MeshListFree(ev); MeshListFree(evo);
+  ierr = PetscFree(feOrient);CHKERRQ(ierr);
+
+  /* Orient Faces with respect to Regions. */
+  iMesh_getEntities(mesh,0,iBase_REGION,iMesh_HEXAHEDRON,&r.v,&r.a,&r.s,&err);CHK(err);
+  iMesh_getAdjEntities(mesh,0,iBase_REGION,iMesh_HEXAHEDRON,iBase_FACE,
+                       &f.v,&f.a,&f.s, &rfo.v,&rfo.a,&rfo.s, &in.v,&in.a,&in.s, &err);CHK(err);
+  MeshListFree(in);
+  iMesh_getEntArrAdj(mesh,r.v,r.s,iBase_VERTEX,&rv.v,&rv.a,&rv.s,&rvo.v,&rvo.a,&rvo.s,&err);CHK(err);
+  iMesh_getEntArrAdj(mesh,f.v,f.s,iBase_VERTEX,&fv.v,&fv.a,&fv.s,&fvo.v,&fvo.a,&fvo.s,&err);CHK(err);
+  printf("r %d, f %d, rv %d, rfo %d, rvo %d, fv %d, fvo %d\n", r.s,f.s,rv.s,rfo.s,rvo.s,fv.s,fvo.s);
+  rfOrientSize = f.s;
+  ierr = PetscMalloc(rfOrientSize*sizeof(DohpOrient),&rfOrient);CHKERRQ(ierr);
+  for (i=0; i<r.s && i<1e8; i++) {
+    printf("region[%d]",i);     /* Vertices of this region */
+    for (j=0; j<8; j++) { if (j%4==0) printf("  "); printf(" %3ld",(long)rv.v[rvo.v[i]+j]); }
+    printf("\n");
+    for (j=0; j<6; j++) {       /* Faces of this region */
+      printf("face[%d][%d] %3ld %3ld %3ld %3ld\n",i,j,(long)fv.v[fvo.v[rfo.v[i]+j]],
+             (long)fv.v[fvo.v[rfo.v[i]+j]+1],(long)fv.v[fvo.v[rfo.v[i]+j]+2],(long)fv.v[fvo.v[rfo.v[i]+j]+3]);
+      ierr = DohpOrientFindPerm_HexQuad(&rv.v[rvo.v[i]],&fv.v[fvo.v[rfo.v[i]+j]],j,&rfOrient[rfo.v[i]+j]);CHKERRQ(ierr);
+    }
+  }
+  iMesh_createTag(mesh,DOHP_ORIENT_REGION_FACE,6*sizeof(DohpOrient),iBase_BYTES,
+                  &rfOrientTag,&err,strlen(DOHP_ORIENT_REGION_FACE));CHK(err);
+  iMesh_setArrData(mesh,r.v,r.s,rfOrientTag,(const char*)rfOrient,rfOrientSize*sizeof(DohpOrient),&err);CHK(err);
+  iMesh_createTag(mesh,DOHP_ADJ_REGION_FACE,6,iBase_ENTITY_HANDLE,&rfAdjTag,&err,strlen(DOHP_ADJ_REGION_FACE));CHK(err);
+  iMesh_setEHArrData(mesh,r.v,r.s,rfAdjTag,f.v,f.s,&err);CHK(err);
+  MeshListFree(r); MeshListFree(f); MeshListFree(rfo); MeshListFree(rv); MeshListFree(rvo);
+  MeshListFree(fv); MeshListFree(fvo);
+  ierr = PetscFree(rfOrient);CHKERRQ(ierr);
+
+  iMesh_save(mesh,0,outfile,outopts,&err,strlen(outfile),strlen(outopts));CHK(err);
+  ierr = PetscFinalize();CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
