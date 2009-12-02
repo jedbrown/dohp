c6d875c57c11f728d262eb374719f166ccefe16c
Jed Brown <jed@59A2.org>
2009-12-02 20:56:12 +0100

    Keep track of Dohp's Initialized state (still private for now).

--- src/util/dohpinit.c
+++ src/util/dohpinit.c
@@ -2,13 +2,17 @@
 #include <dohpsys.h>
 #include <dohp.h>
 
+static bool DohpInitialized;
+
 dErr dInitialize(int *argc,char ***argv,const char *file,const char *help)
 {
   dErr err;
 
   dFunctionBegin;
+  if (DohpInitialized) dFunctionReturn(0);
   err = PetscInitialize(argc,argv,file,help);dCHK(err);
   err = dViewerRegisterAll(NULL);dCHK(err);
+  DohpInitialized = true;
   dFunctionReturn(0);
 }
 
@@ -17,6 +21,12 @@ dErr dFinalize(void)
   dErr err;
 
   dFunctionBegin;
+  if (!DohpInitialized) {
+    (*PetscErrorPrintf)("dInitialize() must be called before dFinalize()\n");
+    PetscFunctionReturn(0);
+  }
+  /* Don't unregister our viewers, PETSc will take care of it */
   err = PetscFinalize();dCHK(err);
+  DohpInitialized = false;
   dFunctionReturn(0);
 }
