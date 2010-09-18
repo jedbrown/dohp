e46fa18c46d336cf385a71036d31261e71cd4cc6
Jed Brown <jed@59A2.org>
2010-09-19 01:41:10 +0200

    Bring fscoord.c into fsgeom.c, don't need both of these

--- src/fs/interface/fsgeom.c
+++ src/fs/interface/fsgeom.c
@@ -111,6 +111,35 @@ static dErr dFSCreateGeometryFromMesh_Private(dFS fs)
   dFunctionReturn(0);
 }
 
+dErr dFSGetCoordinateFS(dFS fs,dFS *incfs)
+{
+  dErr err;
+
+  dFunctionBegin;
+  if (!fs->geometry.fs) {
+    dFS cfs;
+    dMesh mesh;
+    dJacobi jacobi;
+    dMeshTag dtag;
+    char degreename[256],prefix[256];
+
+    err = dFSGetMesh(fs,&mesh);dCHK(err);
+    err = dFSGetJacobi(fs,&jacobi);dCHK(err);
+    err = dFSCreate(((dObject)fs)->comm,&cfs);dCHK(err);
+    err = dFSSetMesh(cfs,mesh,fs->set.active);dCHK(err);
+    err = PetscSNPrintf(degreename,sizeof degreename,"%scfs_degree",((dObject)fs)->prefix);dCHK(err);
+    err = dMeshCreateRuleTagIsotropic(mesh,fs->set.active,degreename,dPolynomialOrderCreate(0,1,1,1),&dtag);dCHK(err);
+    err = dFSSetDegree(cfs,jacobi,dtag);dCHK(err);
+    err = dFSSetRuleTag(cfs,jacobi,dtag);dCHK(err); /* FIXME: remove this attribute from dFS */
+    err = dFSSetBlockSize(cfs,3);dCHK(err);
+    err = PetscSNPrintf(prefix,sizeof prefix,"%scoord_",((dObject)fs)->prefix);dCHK(err);
+    err = dFSSetOptionsPrefix(cfs,prefix);dCHK(err);
+    err = dFSSetFromOptions(cfs);dCHK(err);
+    fs->geometry.fs = cfs;
+  }
+  *incfs = fs->geometry.fs;
+  dFunctionReturn(0);
+}
 
 /** Get displacements of every node in expanded vector.
 *
@@ -134,7 +163,6 @@ dErr dFSGetGeometryVectorExpanded(dFS fs,Vec *ingeom)
   *ingeom = fs->geometry.expanded;
   dFunctionReturn(0);
 }
-
 
 /** Get the number of subelements and number of vertices of subelements.
 *
