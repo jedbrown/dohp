a5f169087bee38a26af0b3660eef20431a0185dd
Jed Brown <jed@59A2.org>
2011-06-02 16:48:15 +0200

    Print dimensionalized artificial diffusivity in vht summary

--- src/fs/tests/vht.c
+++ src/fs/tests/vht.c
@@ -518,6 +518,7 @@ static dErr VHTView(VHT vht,PetscViewer viewer)
     err = PetscViewerASCIIPrintf(viewer,"Reference viscosity  : %G %s\n",dUnitDimensionalize(u->Viscosity,rheo->B0),  dUnitName(u->Viscosity));dCHK(err);
     err = PetscViewerASCIIPrintf(viewer,"Thermal diffusivity  : %G %s\n",dUnitDimensionalize(u->Diffusivity,rheo->k_T/(rheo->rhoi*rheo->c_i)),dUnitName(u->Diffusivity));dCHK(err);
     err = PetscViewerASCIIPrintf(viewer,"Hydraulic diffusivity: %G %s\n",dUnitDimensionalize(u->Diffusivity,rheo->kappa_w/rheo->rhoi),dUnitName(u->Diffusivity));dCHK(err);
+    if (rheo->Kstab > 0) {err = PetscViewerASCIIPrintf(viewer,"Artificial diffusivity: %G %s\n",dUnitDimensionalize(u->Diffusivity,rheo->Kstab),dUnitName(u->Diffusivity));dCHK(err);}
   }
   err = PetscViewerASCIIPopTab(viewer);dCHK(err);
   dFunctionReturn(0);
