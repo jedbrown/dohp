4ef5ebf47c26b2acedebee186025d97093d2415f
Jed Brown <jed@59A2.org>
2011-10-21 11:17:41 -0500

    Add hook to TestWithReference.sh so I can update test output with DIFF=diffupdate

--- TestWithReference.sh
+++ TestWithReference.sh
@@ -7,9 +7,10 @@ mpi_np="$2"
 refout="$3"
 name="$4"
 shift 4
+[ ${DIFF} ] || DIFF="diff"
 
 tmpout="${name}.tmp"
 echo :: "${mpiexec}" -n ${mpi_np} "./${name}" "$@" || exit 1
 "${mpiexec}" -n ${mpi_np} "./${name}" "$@" > "${tmpout}" || exit 1
-diff -u ${refout} ${tmpout} || exit 1
-rm "${tmpout}"
+${DIFF} -u ${refout} ${tmpout} || exit 1
+rm -f "${tmpout}"
